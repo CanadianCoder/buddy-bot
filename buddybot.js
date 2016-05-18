b1774fcd3089775874a2a59caa3d0e60105d03ac
Simon Duchastel <Simon Duchastel>
2016-05-18 15:06:44 -0700

    Changed token-handling

--- buddybot.js
+++ buddybot.js
@@ -6,7 +6,7 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-    token: 'xoxb-43441349009-rjzcDjio5yPhjxSpUst067sq'
+    token: process.env.token
 }).startRTM();
 
 controller.hears(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
