51606e5e0c89800ae707b8d5a08a2df8e5f13ec1
Simon Duchastel <Simon Duchastel>
2016-05-18 16:56:42 -0700

    Added icon and background image

--- buddybot.js
+++ buddybot.js
@@ -2,6 +2,7 @@ var Botkit = require('botkit')
 var token = process.env.SLACK_TOKEN
 
 var controller = Botkit.slackbot({
+    retry: Infinity,
     debug: false
 });
 
@@ -11,12 +12,14 @@ var bot = controller.spawn({
   if (err) {
     throw new Error(err)
   }
+
+  console.log('Connected to Slack')
 });
 
-controller.hears(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-	, 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['.*']
+	, ['direct_message'], function(bot, message) {
 
     controller.storage.users.get(message.user, function(err, user) {
-        bot.reply(message, 'lol');
+        bot.reply(message, );
     });
 });
