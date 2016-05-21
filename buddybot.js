2895cdf3c64c631aa16167e8871c223cb1732aaf
Simon Duchastel <Simon Duchastel>
2016-05-20 20:19:21 -0700

    Added username identification

--- buddybot.js
+++ buddybot.js
@@ -1,5 +1,6 @@
 var Botkit = require('botkit')
 var token = process.env.SLACK_TOKEN
+var request = require('request');
 
 var controller = Botkit.slackbot({
     retry: Infinity,
@@ -14,16 +15,45 @@ var bot = controller.spawn({
   }
 
   console.log('Connected to Slack')
+})
+
+//gets real name from JSON returned from slack
+function getUsername(userID, callback) {
+  request({ 
+      url: 'https://slack.com/api/users.info',
+      method: 'POST',
+      form: {
+          token: token,
+          user: userID
+      }
+    }, function(error, response, body){
+    if(error) {
+        console.log(error);
+    } else {
+        var username = JSON.parse(body).user.real_name;
+        callback(username);
+    }
+  });
+}
+
+//responds whenever anyone says anything
+controller.hears(['.*'], ['direct_message'], function(bot, message) {
+  var username = getUsername(message.user, function (username) {
+    var reply = "";
+
+    if(username == "Gokul Gowri"){
+      reply = "Stop being so cancerous, Gokul"
+    }
+    else{
+      reply = "Good to know, " + "<@" + message.user + ">";
+    }
+
+    bot.reply(message, reply);
+  });
 });
 
-controller.hears(['.*'], ['direct_message'], function(bot, message) {
-  var reply = "";
-  if(message.user == "gokul_gowri"){
-    reply = "Stop being so cancerous, Gokul"
-  }
-  else{
-    reply = "Thank you for sharing that information with me, " + "<@" + message.user + ">";
-  }
-
+//responds when it's name is mentioned in a channel
+controller.on('direct_mention',function(bot,message) {
+  reply = "Hi there! Don't mind me";
   bot.reply(message, reply);
 });
